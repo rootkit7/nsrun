1cef9b1fa93e8240cf8053d0c2766cb0f16d3499
Thomas Kerpe <toke@toke.de>
2020-04-02 10:16:18 +0200

    Added README, cleanup

--- README.md
+++ README.md
@@ -1,10 +1,47 @@
+# Network namespace runner
+
+Execute program in an existing network namespace.
+
+**Warning:** This program needs elevated permissions to
+do it's job. It also allows to join network namespaces
+of other processes running as root and **also escape them
+again**! It's not a tool to make your processes "secure"!
+
+Make sure to understand the implications.
+
+For most cases tools like firejail, nsenter and others may
+be more capable.
+
+## Why?
+
+The main purpose of this tool is currently to run programs
+within a different network namespace - for example a VPN
+only namespace. This allows to contain the network traffic
+for this program.
 
 
+## Build
+```
+make
+sudo chown root:nsrun nsrun
+sudo chmod 4754 nsrun
+```
+
+Create a group nsrun and add yourself to group nsrun.
 
 
-##
+## Usage
 
-sudo chown root:root nsrun
-sudo chmod 4755 nsrun
+Join a named network namespace:
+```
+NAMESPACE=/var/run/netns/mynamespace
+./nsrun $NAMESPACE /usr/bin/bash -l
+```
 
+Join a namespace of another process
+
+```
+NAMESPACE=/proc/PID/ns/net
+./nsrun $NAMESPACE /usr/bin/bash -l
+```
 
--- nsrun.c
+++ nsrun.c
@@ -8,7 +8,6 @@
 #include <unistd.h>
 
 
-
 #define errExit(msg)    do { perror(msg); exit(EXIT_FAILURE); \
                        } while (0)
 
@@ -16,34 +15,27 @@ int main(int argc, char *argv[])
 {
     int fd;
 
-    //int userid = 1000;
-    //int groupid = 100;
-
     int userid = getuid();
     int groupid = getgid();
-    //fprintf(stderr, "%d", ruid);
     
     if (argc < 3) {
-       fprintf(stderr, "%s /proc/PID/ns/FILE cmd args...\n", argv[0]);
+       fprintf(stderr, "%s /proc/PID/ns/net cmd args...\n", argv[0]);
        exit(EXIT_FAILURE);
     }
 
-    if (setuid(0) == -1)     // Become root
-        errExit("setuid");   // you can set it at run time also
-    fd = open(argv[1], O_RDONLY); /* Get file descriptor for namespace */
+    if (setuid(0) == -1)            /* Become root */
+        errExit("setuid");          /* you can set it at run time also */
+    fd = open(argv[1], O_RDONLY);   /* Get file descriptor for namespace */
     if (fd == -1)
         errExit("open");
 
-    if (unshare(CLONE_NEWNET) == -1)
+    if (unshare(CLONE_NEWNET) == -1)    /* Leave parents namespace */
             errExit("unshare");
 
-    if (setns(fd, CLONE_NEWNET) == -1)       /* Join that namespace */
+    if (setns(fd, CLONE_NEWNET) == -1)  /* Join new namespace */
                errExit("setns");
 
-
-
-
-    if (getuid() == 0) {    
+    if (getuid() == 0) {
         /* process is running as root, drop privileges */
         if (setgid(groupid) != 0)
             errExit("setgid");
